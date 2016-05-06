c36dea445cee684a2f8bf61539c1758d61f63db4
Eudes <aeudes@gmail.com>
2016-05-06 11:19:46 -0300

    ch1 complete

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -7,15 +7,30 @@
             [bouncer.core :as b]
             [bouncer.validators :as v]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:messages (db/get-messages)}))
+    "home.html" (merge {:messages (db/get-messages)}
+                       (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
-(defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page))
-  (POST "/message" request (db/save-message! request)))
+(defn validate-message [params]
+  (first (b/validate
+           params
+           :name v/required
+           :message [v/required [v/min-count 10]])))
 
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+        (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+(defroutes home-routes
+  (GET "/" request (home-page request))
+  (GET "/about" [] (about-page))
+  (POST "/message" request (save-message! request)))
