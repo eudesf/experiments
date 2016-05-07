6daf66e3d3724e24a0ff42b015026af3c21ca709
Eudes <aeudes@gmail.com>
2016-05-07 12:26:06 -0300

    ring-app json return

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.http-response :as response]
             [compojure.core :as compojure]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.format :refer [wrap-restful-format]]))
 
 (defn response-handler [request]
   (response/ok
@@ -10,13 +11,23 @@
          (:remote-addr request)
          "</body></html>")))
 
+(defn wrap-formats [handler]
+  (wrap-restful-format
+   handler
+   {:formats [:json-kw :transit-json :transit-msgpack]}))
+
 (def handler
   (compojure/routes
     (compojure/GET "/" request response-handler)
-    (compojure/GET "/:id" [id] (str "<p>the id is: " id "</p>"))))
-
+    (compojure/GET "/test" [] (response/ok {:test 11}))
+    (compojure/GET "/:id" [id] (str "<p>the id is: " id "</p>"))
+    (compojure/POST "/json" [id] (response/ok {:result id}))
+    ))
+ 
 (defn -main []
   (jetty/run-jetty
-    handler
+   (-> #'handler
+       wrap-formats
+       wrap-reload)
     {:port 3000
      :join? false}))
