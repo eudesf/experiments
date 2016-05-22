1c622c0713ff5547fac4fed34efa2366c1af94cc
Eudes <aeudes@gmail.com>
2016-05-22 16:30:22 -0300

    functional version ok (beta)

--- integrativo-xlsowl/src/integrativo_xlsowl/core.clj
+++ integrativo-xlsowl/src/integrativo_xlsowl/core.clj
@@ -4,22 +4,23 @@
   (:require [clojure.string :as str])
   (:require [clojure.math.combinatorics :as combo]))
 
-;; (def xlsfile "/home/eudes/Downloads/Data_and_Generatorv13.xlsm")
-(def xls-file "/home/eudes/wars/integrativo-xlsowl/resources/Data_and_Generatorv13.xlsm")
+;; (def xls-file "/home/eudes/wars/integrativo-xlsowl/resources/Data_and_Generatorv13.xlsm")
 
-(def column-key-map 
-  {"Protein" :protein-name
-   "Gene" :gene-name
-   "Organism" :organism
-   "Org" :organism
-   "BioProcess" :biological_process
-   "BiologicalProcess" :biological_process
-   "MolecularFunction" :molecular-function
-   "MolFunc" :molecular-function
-   "CellComponent" :cell-component
-   "Comp" :cell-component})
+(def expandmap 
+  {:protein-name ["Protein"]
+   :gene-name ["Gene"]
+   :organism ["Organism" "Org"]
+   :biological-process ["BioProcess" "BiologicalProcess"]
+   :molecular-function ["MolecularFunction" "MolFunc"]
+   :cell-component ["CellComponent" "Comp"]
+   :description ["Situation"]
+   :molecule ["Molecule"]})
 
+(def literal-expandmap 
+  {:molecule "Homocysteine"})
 
+(defn add-literals [data-list]
+  (map (partial merge literal-expandmap) data-list))
 
 (defn split-data-values [data-list]
   (map (fn [data-item]
@@ -34,37 +35,75 @@
        data-list))
 
 (defn combine-final-data [workbook]
-
-  (let [initial-time-millis (System/currentTimeMillis)        
-        final-data (->> (xls/select-sheet "final-data" workbook)
+  (let [final-data (->> (xls/select-sheet "final-data" workbook)
                         (xls/select-columns {:C :protein-name 
                                              :D :gene-name
                                              :E :organism
                                              :F :biological-process
                                              :G :molecular-function
                                              :H :cell-component
-                                             :L :description}))
-        ;; sub-final-data (subvec final-data 7 52)
-        ;; sub-final-data-splited (combine-data-values sub-final-data)
-]
+                                             :L :description}))]
 
     (-> final-data
         (subvec 7 52)
+        (add-literals)
         split-data-values
-        combine-data-values)))
+        combine-data-values
+)))
+
+(defn expand-model [workbook datamap-result]
+  (let [modelmap-all (->> (xls/select-sheet "owl-elements2" workbook)
+                           (xls/select-columns {:B :owl-axiom}))
+        modelmap-body (subvec modelmap-all 29 107)
+        modelmap-heading (subvec modelmap-all 1 29)
+        model-heading (map :owl-axiom modelmap-heading)]
+
+    (cons [(str/join "\n" model-heading)
+           "\n\n<!--==== END OF HEADING ====-->\n"] 
+          (for [datamap-list datamap-result
+                datamap-item datamap-list
+                modelmap modelmap-body
+                :let [result []]]
+            
+            (->> (reduce merge datamap-item)
+                 (expand-model-item modelmap)
+                 (conj result))))))
 
 
-(combine-final-data (xls/load-workbook xls-file))
+(defn expand-model-item [modelmap datamap]
+  (reduce (fn [axiom-str datamap-key]
+            (reduce (fn [axiom-str column]
+                      (str/replace axiom-str
+                                   (re-pattern (str "\\$" column "\\$"))
+                                   (str/replace (datamap-key datamap) #"[^\w\d]" "_")))
+                    axiom-str
+                    (datamap-key expandmap)))
+          
+          (:owl-axiom modelmap)
+          (keys datamap)))
 
-;; sub-owl-elements2 (subvec owl-elements2 29 107)
-;; owl-elements2 (->> (xls/select-sheet "owl-elements2" workbook)
-;;                    (xls/select-columns {:A :id
-;;                                         :B :owl-axiom}))
 
-(defn -main
-  "Application Entry Point"
-  [xlsfile]
+/(defn write-result! [result]
+  (with-open [wtr (clojure.java.io/writer "result.owl")]
+    (binding [*out* wtr]
+      (doseq [result-item result]
+        (apply println result-item))
+      (println "\n</Ontology>"))))
 
-  (if (.exists (io/file xlsfile))
-    (import-file xlsfile)
-    (println "File does not exists!")))
+(defn remove-duplicates [result]
+  (cons (first result) (set (rest result))))
+
+(defn -main []
+  (let [wb (xls/load-workbook xls-file)]
+    (->> 
+     ;; combine all final data values
+     (combine-final-data wb)   
+     
+     ;; expand the varibles in the model with the final data
+     (expand-model wb)         
+
+     remove-duplicates
+
+     ;; write to file
+     write-result!
+     )))
