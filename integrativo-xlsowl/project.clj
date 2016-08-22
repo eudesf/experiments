602fd4a38a8845d5eae2c43722dace474316a86e
Eudes Lima <aeudes@gmail.com>
2016-08-22 11:32:25 -0300

    added core.async and repetition handling by hashes

--- integrativo-xlsowl/project.clj
+++ integrativo-xlsowl/project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [dk.ative/docjure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.2"]
-                 [cprop "0.1.8"]]
+                 [cprop "0.1.8"]
+                 [prismatic/plumbing "0.5.3"]
+                 [org.clojure/core.async "0.2.385"]]
   :main integrativo-xlsowl.core
   :resource-paths ["resources"])
